c87bb860561620fa81051cec1ca1481a780597a6
Mohamed Sayed Amar <106192481+Mohamed-Amar@users.noreply.github.com>
2024-12-22 18:55:51 +0200

    Add files via upload

---
+++ Heap-sort.py
@@ -0,0 +1,30 @@
+def heapify(arr, n, i):
+    largest = i
+    l, r = 2 * i + 1, 2 * i + 2
+
+    if l < n and arr[l] > arr[largest]:
+        largest = l
+
+    if r < n and arr[r] > arr[largest]:
+        largest = r
+
+    if largest != i:
+        arr[i], arr[largest] = arr[largest], arr[i]
+        heapify(arr, n, largest)
+
+def heap_sort(arr):
+    n = len(arr)
+    for i in range(n // 2 - 1, -1, -1):
+        heapify(arr, n, i)
+
+    for i in range(n-1, 0, -1):
+        arr[i], arr[0] = arr[0], arr[i]
+        heapify(arr, i, 0)
+    print(f'The sorted list: {arr}')
+
+lst = []
+size = int(input("Enter the size of the list: "))
+for i in range(size):
+    x = int(input("Enter the element: "))
+    lst.append(x)
+heap_sort(lst)
